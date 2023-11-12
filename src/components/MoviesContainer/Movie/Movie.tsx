3773df1657e221ec267fcb3169b27c16001170b1
Filain <ratecurrent@gmail.com>
2023-11-12 12:35:00 +0200

    end

--- src/components/MoviesContainer/Movie/Movie.tsx
+++ src/components/MoviesContainer/Movie/Movie.tsx
@@ -16,9 +16,6 @@ const Movie:FC<IProps> = ({movie}) => {
 
     const nav = () => {
         navigate(`/info/${id}`)
-
-
-
     }
     return (
         <div className={css.Movie} onClick={nav}>
@@ -27,11 +24,8 @@ const Movie:FC<IProps> = ({movie}) => {
                 <img className={css.img_poster}  src={'https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png'} alt="not found" style={{width:'150'}}/>
             }
             </div>
-
-
             <Rating className={css.Rating} name="customized-10" defaultValue={vote_average} precision={0.1} max={10} size="small" readOnly />
             <p className={css.title}>{original_title}</p>
-
 
         </div>
     );
